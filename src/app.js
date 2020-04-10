8b79bdcb42e288563c07fa21f607c6dacb26e088
Lucas <lucas.raziel@gmail.com>
2020-04-10 20:43:50 -0300

    Challenge Ready

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const {uuid} = require('uuidv4')
 
 // const { uuid } = require("uuidv4");
 
@@ -11,23 +12,52 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const repository = request.body
+
+  repository.id = uuid()
+
+  repository.likes=0
+
+  repositories.push(repository)
+  
+  return response.json(repository)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params
+  const {techs, url, title} = request.body
+  const repositoryIndex = repositories.findIndex(repository=>repository.id===id)
+  if (repositoryIndex<0){
+    return response.status(400).json({error: 'Repository does not exist'})
+  }
+  const {likes} = repositories[repositoryIndex]
+  const repository = {id,techs,url,title,likes}
+  repositories[repositoryIndex] = repository
+  return response.json(repository)
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params
+  const repositoryIndex = repositories.findIndex(repository=>repository.id===id)
+  if (repositoryIndex<0){
+    return response.status(400).json({error: 'Repository does not exist'})
+  }
+  repositories.splice(repositoryIndex,1)
+  return response.status(204).json()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params
+  const repositoryIndex = repositories.findIndex(repository=>repository.id===id)
+  if (repositoryIndex<0){
+    return response.status(400).json({error: 'Repository does not exist'})
+  }
+  repositories[repositoryIndex].likes++
+  return response.json(repositories[repositoryIndex])
 });
 
 module.exports = app;
